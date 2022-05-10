8558f55ff317cafebcfcf0f201c446cf89e4c4cd
Victor <victor.alvesf7@gmail.com>
2022-05-10 18:44:35 -0400

    corrige repository

--- src/main/java/com/spring/elastic/infrastructure/repository/UserSearchRepository.java
+++ src/main/java/com/spring/elastic/infrastructure/repository/UserSearchRepository.java
@@ -1,11 +1,9 @@
 package com.spring.elastic.infrastructure.repository;
 
-import java.util.UUID;
+import com.spring.elastic.domain.model.User;
 
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-import com.spring.elastic.domain.model.User;
-
-public interface UserSearchRepository extends ElasticsearchRepository<User, UUID> {
+public interface UserSearchRepository extends ElasticsearchRepository<User, Long> {
 
 }
